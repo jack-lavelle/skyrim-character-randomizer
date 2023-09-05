9351be6723cc77fc0620abf930517f26bff3d998
Jack LaVelle <jacklavelle17@gmail.com>
2023-09-05 00:10:46 -0400

    skills and background are now linked together, both must be unselected to change

--- application.py
+++ application.py
@@ -2,12 +2,10 @@ from flask import Flask
 from flask import render_template, request
 from ast import literal_eval
 import CharacterBuild
+import attributes
 
 app = Flask(__name__)
-
-# TODO : design page and send data using jinja ... think of skyrim background with smooth white curved rectangle in front and then cool font, size, color for the text.
 # TODO : button for completely new character
-# TODO : background changes even if skills are held constant
 # TODO : do I really need another page for handle_data ... condense it into one page.
 
 
@@ -26,6 +24,13 @@ def handle_data():
     d = {}
     for dict_string in checked_properties:
         d.update(literal_eval(dict_string))
+
+    # If `skills` is selected, then so too should `sackground` be.
+    if "skills" in d:
+        background_val = list(attributes.backgrounds.keys())[
+            list(attributes.backgrounds.values()).index(d["skills"])
+        ]
+        d["background"] = background_val
 
     new_character = CharacterBuild.Character(d)
     new_character.previously_checked_properties = list(d.keys())
